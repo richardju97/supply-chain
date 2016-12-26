9efa82ddb568b0381e9d2977a473265cc80c5b11
rju <rju@purdue.edu>
2016-12-27 00:08:40 +0800

    Malloced Fleet List, addTruck function w/ basic test case script complete

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include "supply-chain.h"
 
+void printFleet() {
+	
+	int i = 0;
+	while (listOfTrucks[i] != NULL) {
+			printf("%s\n", listOfTrucks[i]->licensePlate);  
+		i++;
+	}
+	
+}
+
 int main() {
+
+	createDC();
+	
+	char * lp1 = "This is License Plate 1";
+	addTruck(lp1);
+	
+	char * lp2 = "License Plate Number 2";
+	addTruck(lp2);
+	
+	printFleet();
+
 	return 0;
 }
+
--- supply-chain.c
+++ supply-chain.c
@@ -5,17 +5,36 @@
  	totalTrucks = 0;
  	numProducts = 0;
  	
- 	//need to malloc listOfTrucks and dcInventory
+ 	trucksPerList = 10;
+ 	
+ 	//need to malloc dcInventory
+ 	listOfTrucks = malloc(sizeof(truck*)*trucksPerList);
+ 	int i;
+ 	for (i = 0; i < trucksPerList; i++) {
+ 		listOfTrucks[i] = NULL;
+ 	}	
  }
  
- int addItemByName(char * name) {
+ int addItemByName(char * name, double MSRP) {
  	
- 	
- 	
+ 	item *newItem = malloc(sizeof(item));
+ 	newItem->name = strdup(name);
+	newItem->MSRP = MSRP;
+ 	 	
+ 	 // need to add to dcInventory
+ 	 	
  	return 0;
  } 
  
- int addTruck() {
+ int addTruck(char * lp) {
  
+ 	truck * newTruck = malloc(sizeof(truck));
+ 	newTruck->licensePlate = strdup(lp);
+	newTruck->status = DC;
+	
+	// need to add to listOfTrucks
+	listOfTrucks[totalTrucks] = newTruck;
+	totalTrucks++;
+	
  	return 0;
  }
