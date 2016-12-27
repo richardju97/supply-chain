9fc0a9588718402920435019b41dccab61dc5d01
rju <rju@purdue.edu>
2016-12-28 00:29:23 +0800

    Updated with additional test cases for items

--- main.c
+++ main.c
@@ -9,8 +9,16 @@ void printFleet() {
 	while (listOfTrucks[i] != NULL) {
 			printf("Truck ID: %d\n", listOfTrucks[i]->ID);  
 			printf("License Plate: %s\n", listOfTrucks[i]->licensePlate);  
-			printf("Current Status: %u\n\n", listOfTrucks[i]->status); 
-// 			printf("Current Cargo: %s\n\n", listOfTrucks[i]->licensePlate);   
+			printf("Current Status: %u\n\n", listOfTrucks[i]->status);
+			
+			// if (listOfTrucks[i]->shipment[0] != NULL) {
+// 			
+// // 				only prints the first item as of now
+// 				printf("Current Cargo: %s\n\n", listOfTrucks[i]->shipment[0]->name);   
+// 			} else {
+// 				printf("No cargo on board.\n\n");
+// 			}
+			
 		i++;
 	}
 	
@@ -18,9 +26,28 @@ void printFleet() {
 	printf("---------------------\n\n");  
 }
 
+void printDCInventory() {
+
+	printf("\n---------------------\n");  
+	printf("Printing DC Inventory (All Items):\n\n");  
+	
+	int i = 0;
+	while (listOfTrucks[i] != NULL) {
+// 			printf("Truck ID: %d\n", listOfTrucks[i]->ID);  
+			printf("Item Name: %s\n", dcInventory[i]->name);  
+			printf("MSRP: $%.2f\n\n", dcInventory[i]->MSRP); 
+		i++;
+	}
+	
+	printf("End of DC Inventory.\n");  
+	printf("---------------------\n\n");  	
+}
+
 int main() {
 
 	createDC();
+	
+	// basic test case for trucks
 	
 	char * lp1 = "This is License Plate 1";
 	addTruck(lp1);
@@ -28,7 +55,20 @@ int main() {
 	char * lp2 = "License Plate Number 2";
 	addTruck(lp2);
 	
+	removeTruck(1);
+	
 	printFleet();
+	
+	// basic test case for items
+	
+	char * item1 = "Some Item to be sold";
+	addItemByName(item1, 4.99);
+	
+	char * item2 = "Please buy this product";
+	addItemByName(item2, 10.99);
+
+	printDCInventory();
+	
 
 	return 0;
 }
